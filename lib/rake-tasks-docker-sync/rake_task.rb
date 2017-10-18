990608b6d5aa68deefd3e532c5efd47adcfb2d45
Kieren Evans <kevans@inviqa.com>
2017-10-18 11:23:02 +0100

    Don't conflict with rake-tasks-docker

--- lib/rake-tasks-docker-sync/rake_task.rb
+++ lib/rake-tasks-docker-sync/rake_task.rb
@@ -2,17 +2,17 @@ require_relative 'services'
 
 namespace :docker do
   namespace :sync do
-    def services_from_args(args)
+    def sync_services_from_args(args)
       RakeTasksDockerSync::Services.new(args[:name] ? args[:name].split(' ') : [])
     end
 
     task :start, :name do |task, args|
-      services = services_from_args(args)
+      services = sync_services_from_args(args)
       services.up
     end
 
     task :clean, :name do |task, args|
-      services = services_from_args(args)
+      services = sync_services_from_args(args)
       services.clean
     end
   end
